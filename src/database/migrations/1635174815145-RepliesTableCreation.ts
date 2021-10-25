cca2723650e8824aadf2ce679a85782b05dfd714
Brian <>
2021-10-25 18:29:08 +0300

    - add migration for replies table

---
+++ src/database/migrations/1635174815145-RepliesTableCreation.ts
@@ -0,0 +1,42 @@
+import {MigrationInterface, QueryRunner, Table, TableColumn} from "typeorm";
+
+export class RepliesTableCreation1635174815145 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+              name: 'replies',
+              columns: [
+                new TableColumn({
+                  name: 'id',
+                  isNullable: false,
+                  isPrimary: true,
+                  isGenerated: true,
+                  default: 'uuid_generate_v4()',
+                  type: 'uuid',
+                }),
+                new TableColumn({
+                  name: 'name',
+                  isNullable: true,
+                  type: 'varchar',
+                }),
+                new TableColumn({
+                  name: 'description',
+                  isNullable: true,
+                  type: 'varchar',
+                }),
+                new TableColumn({
+                  name: 'reply',
+                  isNullable: true,
+                  type: 'varchar',
+                }),
+              ],
+            }),
+          );
+        }
+      
+        public async down(queryRunner: QueryRunner): Promise<any> {
+          await queryRunner.dropTable('replies');
+        }
+      }
+      
