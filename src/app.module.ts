533234e300739ef6d5cca02ac5431fb6fb86b19d
Brian <>
2021-10-25 17:48:44 +0300

    - create app modules

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppService } from './app.service';
+import { ConfigurationModule } from './common/configurations/configurations.module';
+import { TypeOrmConfigService } from './common/configurations/type-orm-config.service';
+import { ReplyModule } from './modules/comments/replies.module';
+
+@Module({
+  imports: [ReplyModule, TypeOrmModule.forRootAsync({
+    imports: [ConfigurationModule],
+    useExisting: TypeOrmConfigService,
+  }),],
+  providers: [AppService],
+})
+export class AppModule {}
